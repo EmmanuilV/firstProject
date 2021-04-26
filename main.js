bd24f770d1f77242de18af82027909da1a37ca26
EmmanuilV <emmanuilva@tahometer.com>
2021-04-26 11:39:38 +0300

    append new tasks

--- main.js
+++ main.js
@@ -1,90 +1,124 @@
 class Task {
-    constructor(title, description, done, dueDate) {
+    constructor(id, title, description, done, dueDate) {
+        this.id = id;
         this.title = title;
         this.description = description;
         this.done = done;
-        this.dueDate = dueDate;
+        this.dueDate = new Date(dueDate);
     }
 }
 
-let index = 1;
 const todoItem = document.querySelector('main');
 let todoList = [
-    new Task("Make Breakfast", "meat, vegetable", false, '2021-04-18'),
-    new Task("Make Dinner", "meat, rice", true, '2021-04-20'),
-    new Task("Make Supper", "meat, potato", false, '2021-04-25'),
-    new Task("Make Breakfast", "meat, vegetable", false, '2021-04-18'),
-    new Task("Make Dinner", "meat, rice", true, '2021-04-20'),
-    new Task("Make Supper", "meat, potato", false, '2021-04-25'),
-    new Task("Make Breakfast", "meat, vegetable", false, '2021-04-18'),
-    new Task("Make Dinner", "meat, rice", true, '2021-04-20'),
-    new Task("Make Supper", "meat, potato", false, '2021-04-25')
+    new Task(1, "1Make Breakfast", "meat, vegetable", false, '2021-04-18'),
+    new Task(2, "2Make Dinner", "meat, rice", true, '2021-04-20'),
+    new Task(3, "3Make Supper", "meat, potato", false, '2021-04-25'),
+    new Task(4, "4Make Breakfast", "meat, vegetable", false, '2021-04-18'),
+    new Task(5, "5Make Dinner", "meat, rice", true, '2021-04-20'),
+    new Task(6, "6Make Supper", "meat, potato", false, '2021-04-25'),
+    new Task(7, "7Make Breakfast", "meat, vegetable", false, '2021-04-18'),
+    new Task(8, "8Make Dinner", "meat, rice", true, '2021-04-20'),
+    new Task(9, "9Make Supper", "meat, potato", false, '2021-04-25')
 ]
 
+
+
+let taskForm = document.forms['task'];
+
+taskForm.addEventListener('submit', (event) => {
+    let lastId;
+    for (let i = 0; i < todoList.length; i++) {
+        if (todoList.length > 0) {
+            lastId = todoList[i].id;
+            lastId++;
+        }
+    }
+    event.preventDefault();
+    const formData = new FormData(taskForm);
+    let obj = Object.fromEntries(formData.entries());
+    const task = new Task(lastId, obj.title, obj.description, false, obj.dueDate);
+    todoList.push(task);
+    appendTask(task);
+    taskForm.reset();
+    console.log("Created: " + lastId, todoList[lastId - 1]);
+})
+
 function deleteTask(target) {
-    target.parentElement.remove();
     let id = target.parentElement.id;
-    delete todoList[id - 1];
-    console.log(id, todoList);
-}
+    for (let i = 0; i < todoList.length; i++) {
+        if (todoList[i].id == id) {
+            console.log("deleted: " + id, todoList[i]);
 
-function generateId(task) {
-    appendTask(task, index);
-    index++;
-}
-
-/* function completeTask(target) {
-    console.log(target.closest('SECTION'))
-    target.closest('SECTION').style.display = 'none';
-} */
-
-function completeTask(target) {
-    let id = target.parentElement.parentElement.id;
-    if (todoList[id - 1].done) {
-        todoList[id - 1].done = false;
-        target.parentElement.classList.remove("task-complete");
-    } else {
-        todoList[id - 1].done = true;
-        target.parentElement.classList.add("task-complete");
+            todoList.splice(i, 1);
+            target.parentElement.remove();
+        }
     }
 }
 
-function showAllTasks(target) {
+function completeTask(target) {
+    // let id = target.parentElement.parentElement.id;
+    // console.log("Start info: " + id + " - " + todoList[id - 1].done);
+
+    // if (todoList[id - 1].done) {
+    //     todoList[id - 1].done = false;
+    //     target.parentElement.classList.remove("task-complete");
+    //     console.log(todoList[id - 1].done);        
+    // } else {
+    //     todoList[id - 1].done = true;
+    //     target.parentElement.classList.add("task-complete");
+    //     console.log("Task Done inside:" + todoList[id - 1].done);
+    // }
+    // console.log("Task Done outside:" + todoList[id - 1].done);
+    // console.log(todoList);
+    let titleBlock = target.closest('DIV');
+    titleBlock.classList.toggle('task-complete')
+}
+
+function hideTasks(target) {
     let section = document.querySelectorAll('section');
-    for (let i = 0; i < section.length; i++) {
-        if(todoList[i].done) {
+    for (let i = 0; i < todoList.length; i++) {
+        if (todoList[i] !== undefined && todoList[i].done) {
+            console.log(section[i].id, todoList[i]);
             section[i].style.display = 'none';
         }
     }
 }
 
-function hideTask(done) {
+function showAllTasks(target) {
     let section = document.querySelectorAll('section');
-    for (let i = 0; i < section.length; i++) {
-        if(todoList[i].done) {
+    for (let i = 0; i < todoList.length; i++) {
+        if (todoList[i] !== undefined && todoList[i].done) {
+            console.log(section[i].id, todoList[i]);
             section[i].style.display = 'flex';
         }
     }
 }
 
-function appendTask(task, index) {
-    const { title, description, done, dueDate } = task;
-    let date = new Date(dueDate);
-    let dateStrFormat = `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`
+
+
+function appendTask(task) {
+    const { id, title, description, done, dueDate } = task;
     todoItem.innerHTML +=
-        `<section id="${index}">` +
+        `<section id="${id}">` +
         `<button onclick="deleteTask(event.target)">&#735</button>` +
-            `<div class="title ${isCompleteForTitle(done)}">` +
-            `<input type="checkbox"  ${isCompleteForInput(done)} onclick="completeTask(event.target)"/>` +
-            `<h3>${title}</h3>` +
+        `<div class="title ${isCompleteForTitle(done)}">` +
+        `<input type="checkbox"  ${isCompleteForInput(done)} onclick="completeTask(event.target)"/>` +
+        `<h3>${title}</h3>` +
         `</div>` +
         `<div class="info">` +
-            `<p>${description}</p>` +
-            `<p ${checkDate(dueDate, done)}>${getDueDate(dateStrFormat)}</p>` +
+        `<p>${emptyDescription(description)}</p>` +
+        `<p ${checkDate(dueDate, done)}>${getDueDate(dueDate)}</p>` +
         `</div>` +
         `</section>`;
 }
 
+function emptyDescription(description) {
+    if (description == undefined ) {
+        return '';
+    } else {
+        return description;
+    }
+}
 
 function isCompleteForTitle(done) {
     if (done) {
@@ -101,10 +135,11 @@ function isCompleteForInput(done) {
 }
 
 function getDueDate(dueDate) {
-    if (dueDate != "" && dueDate != undefined) {
-        return dueDate;
-    } else {
+    if (dueDate === "" || dueDate === undefined || dueDate === null) {
         return "";
+    } else {
+        return dueDate.toDateString();
+
     }
 }
 
@@ -120,4 +155,4 @@ function checkDate(dueDate, done) {
 
 }
 
-todoList.forEach(generateId);
+todoList.forEach(appendTask);
